c8c8f20b21905c6cbbdf6d2cdb878ccb0119c94c
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2021-10-01 15:06:04 +0900

    1001

--- PR01-01-19013135/src/JC19013135PR0101.java
+++ PR01-01-19013135/src/JC19013135PR0101.java
@@ -2,26 +2,27 @@
 public class JC19013135PR0101 {
 	public static void main(String args[])
 	{
-		int intArray[][] = new int[4][4];
 		
-		int x, y, cnt=0;
+		int array[][] = new int[4][4];
+		int num = 0;
 		
-		while(cnt<10)
+		while(true)
 		{
-			x = (int)(Math.random()*4);
-			y = (int)(Math.random()*4);
-			if(intArray[x][y]==0)
-			{
-				intArray[x][y] = (int)(Math.random()*10+1);
-				cnt++;
+			int x,y;//x, y축 선언
+			x = (int)(Math.random()*4);//0~4까지의 x축
+			y = (int)(Math.random()*4);//0~4까지의 y축
+			if(array[x][y]==0) {
+				array[x][y] = (int)(Math.random()*10+1);//1~10까지의 수 입력
+				num++;//채워진 숫자의 개수 카운팅
 			}
+			if(num == 10) break;//10개의 숫자를 채웠으면 종료
 		}
 		
-		for(int i=0; i<intArray.length; i++)
+		for(int i=0; i<array.length; i++)//배열 출력
 		{
-			for(int j=0; j<intArray[i].length; j++)
+			for(int j=0; j<array[i].length; j++)
 			{
-				System.out.print(intArray[i][j]+" ");
+				System.out.print(array[i][j]+" ");
 			}
 			System.out.println();
 		}
--- PR01-02-19013135/src/JC19013135PR0102.java
+++ PR01-02-19013135/src/JC19013135PR0102.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 class Phone{
 	private String name;
-	private String number;
+	private String number;//이름과 전화번호
 	
 	Phone(String name, String number)
 	{
-		this.name = name;
+		this.name = name;//입력받은 이름, 번호을 Phone객체로 넘겨준다
 		this.number = number;
 	}
 	
@@ -19,23 +19,21 @@ class Phone{
 		return number;
 	}
 }
-public class JC19013135PR0102 {
+public class JC19013135PR0102 {//PhoneBook클래스
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
-		Phone people[];
-		int n;
 		
 		System.out.print("인원수>>");
-		n = sc.nextInt();
-		
-		people = new Phone[n];
+		int n = sc.nextInt();//인원수 입력
+		Phone[] people = new Phone[n];//인원수에 맞는 Phone객체 배열 생성
 		
 		for(int i=0; i<people.length; i++)
 		{
 			System.out.print("이름과 전화번호(이름과 번호는 빈 칸없이 입력)>>");
-			people[i] = new Phone(sc.next(), sc.next());
+			people[i] = new Phone(sc.next(), sc.next());//이름과 전화번호 string입력
 		}
+		
 		System.out.println("저장되었습니다...");
 		
 		while(true)
@@ -43,20 +41,20 @@ public class JC19013135PR0102 {
 			System.out.println("검색할 이름>>");
 			String name = sc.next();
 			boolean found = false;
-			if(name.equals("그만")) break;
+			
+			if(name.equals("그만")) break;//그만이 입력 되면 종료
+			
 			for(int i=0; i<people.length; i++)
 			{
-				if(name.equals(people[i].getName())) {
+				if(name.equals(people[i].getName())) {//i번째 people객체에 동일한 이름 존재 할시
 					System.out.println(name+"의 번호는 "+people[i].getNumber()+" 입니다");
-					found = true;
+					found = true;//이름을 찾았으므로 true로 변경하고 종료
 					break;
 				}
 			}
-			if(!found)
-			{
+			if(!found)//동일한 이름이 존재하지 않을 때
 				System.out.println(name+" 이 없습니다.");
-			}
 		}
-		sc.close();
+		sc.close();//Scanner sc 객체 free
 	}
 }

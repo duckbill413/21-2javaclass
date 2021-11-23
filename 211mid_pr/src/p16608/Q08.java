3f7921f1b9dc0a27e03801ed81680e1c294947f0
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2021-11-23 18:49:17 +0900

    1123 java vscode 추가

--- 211mid_pr/src/p16608/Q08.java
+++ 211mid_pr/src/p16608/Q08.java
@@ -1,37 +1,36 @@
 package p16608;
+
 import java.util.Scanner;
 
 public class Q08 {
-	public static void main(String [] args)
-	{
+	public static void main(String[] args) {
 		System.out.print("정수 몇개?");
 		Scanner sc = new Scanner(System.in);
 		int cnt = sc.nextInt();
 
-		int num [] = new int [cnt];
+		int num[] = new int[cnt];
 		int size = 0;
-		while(true)
-		{
-			int tmp = (int)(Math.random()*100 + 1);
+		while (true) {
+			int tmp = (int) (Math.random() * 100 + 1);
 			int found = 0;
-			for(int i=0; i<num.length; i++)
-			{
-				if(num[i]==tmp)
-				{
+			for (int i = 0; i < num.length; i++) {
+				if (num[i] == tmp) {
 					found = 1;
 					break;
 				}
 			}
-			if(found==0) {
-				num[size]= tmp;
+			if (found == 0) {
+				num[size] = tmp;
 				size++;
 			}
-			if(size == cnt) break;
+			if (size == cnt)
+				break;
 		}
 
-		for(int i=1; i<=num.length; i++) {
-			System.out.print(num[i-1]+" ");
-			if(i%10==0) System.out.println();
+		for (int i = 1; i <= num.length; i++) {
+			System.out.print(num[i - 1] + " ");
+			if (i % 10 == 0)
+				System.out.println();
 		}
 	}
 }
